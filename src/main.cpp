89e3f19c471a4c3dfc1bc4ee146475fe6762f35d
Stijn Kammer <stijnberend@hotmail.com>
2025-03-18 19:56:39 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <WiFi.h>
+
+// Replace with your network credentials
+const char* ssid     = "Project-Beer2";
+const char* password = "123456789";
+
+// // Set web server port number to 80
+// WiFiServer server(80);
+
+void setup() {
+  // Serial.begin(115200);
+
+  // Serial.print("Setting AP (Access Point)…");
+  WiFi.softAP(ssid, password);
+
+  // IPAddress IP = WiFi.softAPIP();
+  // Serial.print("AP IP address: ");
+  // Serial.println(IP);
+  
+  // server.begin();
+}
+
+void loop() {
+}
