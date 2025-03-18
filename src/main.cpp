0b2599244bf0d667a0a54dc0d81915a0d7596635
Stijn Kammer <stijnberend@hotmail.com>
2025-03-18 20:25:00 +0100

    Added in webserver

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,19 @@ const char* ssid     = "Project-Beer2";
 const char* password = "123456789";
 
 // // Set web server port number to 80
-// WiFiServer server(80);
+WiFiServer server(80);
 
 void setup() {
-  // Serial.begin(115200);
+  Serial.begin(115200);
 
-  // Serial.print("Setting AP (Access Point)…");
+  Serial.print("Setting AP (Access Point)…");
   WiFi.softAP(ssid, password);
 
-  // IPAddress IP = WiFi.softAPIP();
-  // Serial.print("AP IP address: ");
-  // Serial.println(IP);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
   
-  // server.begin();
+  server.begin();
 }
 
 void loop() {
